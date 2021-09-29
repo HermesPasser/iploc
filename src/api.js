1739712138d6fe76ad008fd1f971e6eaa5128483
Douglas Lacerda <dsldouglas@hotmail.com>
2021-09-29 11:06:19 -0300

    add interface with API's endpoint that returns one's own ip

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const KEY = ''
+const KEY = 'at_vYykeGb0OiAURnAfw83W1YACNlH9t'
 const API = `https://geo.ipify.org/api/v1?apiKey=${KEY}`
 const MOCKING = false
 
@@ -10,12 +10,22 @@ async function _get(ip) {
     return data
 }
 
+async function _getOwn() {
+    const response = await fetch(`${API}`)
+    if (!response)
+        return null
+    const data = response.json()
+    return data
+}
+
 async function _getMock(ip) {
     return {"ip":"8.8.8.8","location":{"country":"US","region":"California","city":"Mountain View","lat":37.38605,"lng":-122.08385,"postalCode":"94035","timezone":"-07:00","geonameId":5375480},"domains":["0--9.ru","000180.top","00049ok.com","000xs.net","001998.com.he2.aqb.so"],"as":{"asn":15169,"name":"Google LLC","route":"8.8.8.0\/24","domain":"https:\/\/about.google\/intl\/en\/","type":"Content"},"isp":"Google LLC","proxy":{"proxy":false,"vpn":false,"tor":false}}
 }
 
-export default async function get(ip) {
+export async function get(ip) {
     return MOCKING? await _getMock(ip) : await _get(ip)
 }
 
- 
+export async function getOwn() {
+    return MOCKING? await _getMock('ip') : await _getOwn()
+}
