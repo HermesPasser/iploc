f59e5bb3021686249b3c7878da178d563f318ee8
Douglas Lacerda <dsldouglas@hotmail.com>
2021-09-29 10:15:41 -0300

    created Map component

---
+++ src/comps/Map.js
@@ -0,0 +1,28 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useMap } from 'react-leaflet'
+
+export default  function Map({coord, name}) {
+    
+    function ChangeMapView({ coords }) {
+        const map = useMap();
+        map.setView(coords, map.getZoom());
+      
+        console.log(coords)
+        return null;
+    }
+    
+	return ( 
+        <MapContainer center={coord} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={coord}>
+                <Popup>{name}
+                </Popup>
+            </Marker>
+            
+            <ChangeMapView coords={coord} zoom={13} />
+        </MapContainer>
+	)
+}
